1dd0ab5451cd79b7cc66b53b1d553b9ebefc2d43
Pavo <wamjsblvb@live.com.mx>
2023-11-09 14:05:29 -0600

    Clustering en tipo de orden

--- clusterordtype.py
+++ clusterordtype.py
@@ -23,9 +23,14 @@ def order_type_clustering(points: list, k: int) -> dict:
         return 'izq' if det > 0 else 'der'  # En sentido horario o antihorario
 
     def polar_binary_search(points, p):
-
-        # Realiza una búsqueda binaria en la lista ordenada de puntos en sentido polar en O(log n)
         
+        '''
+            Realiza una búsqueda binaria en la lista ordenada de puntos en sentido polar en O(log n)
+            -Parametrs:
+            points: muestra de puntos
+            p: punto a buscar
+        
+        '''
         left, right = 0, len(points) - 1
         
         while left <= right:
@@ -47,13 +52,19 @@ def order_type_clustering(points: list, k: int) -> dict:
         return -1  # El punto no se encontró en la lista
 
     def ordered_points(points: list) -> dict:
+        
+        '''
+            
+            Método para realizar el ordenamiento respecto a ángulo de todos los puntos.
+            Devuelve un diccionario cuyas claves son las tuplas de los vectores para acceder al orden respecto al ángulo.
+            Este método es O(n^{2}\log n).
 
-        # Método para realizar el ordenamiento respecto a ángulo de todos los puntos.
-        # Devuelve un diccionario cuyas claves son las tuplas de los vectores para acceder al orden respecto al ángulo.
-        # Este método es O(n^{2}\log n).
+            Uso de un diccionario para poder accesar al orden de cada punto x así como los ángulos respecto a x.
 
-        # Uso de un diccionario para poder accesar al orden de cada punto x así como los ángulos respecto a x.
-
+            -Parametros:
+            points: muestra de puntos donde se hará el ordenamiento polar.
+        
+        '''
         ordered_points = dict()
 
         for x in points:
@@ -78,9 +89,18 @@ def order_type_clustering(points: list, k: int) -> dict:
 
     def delta(p: list, q: list, points: list, ordered_points: dict) -> int:
         
-        # Función para el cálculo de la distancia en "tipo de orden".
-        # Este método es O(n\log n).
+        '''
+        
+            Función para el cálculo de la distancia en "tipo de orden".
+            Este método es O(n\log n).
 
+            -Parametros: 
+            p: punto en points
+            q: punto en points
+            points: muestra de puntos donde se calculará delta
+            ordered_points: Diccionario que almacenará la información de ordenamiento respecto a points.
+        
+        '''
         # Condición de distancia para puntos fuera de "points"
 
         if (q not in points) or (p not in points):
@@ -95,9 +115,9 @@ def order_type_clustering(points: list, k: int) -> dict:
 
             # Lista que consiste en los puntos distintos a p y q.
 
-            not_p_not_q = [point for point in points if point != p and point != q]
+            npnq = [point for point in points if point != p and point != q]
 
-            for x in not_p_not_q:
+            for x in npnq:
                     
                     # sapax = sort all points by angle from x
 
@@ -129,29 +149,60 @@ def order_type_clustering(points: list, k: int) -> dict:
 
             return int(distance / 2)
     
-    def centroid(cluster: list) -> list:
-
-        # Función para calcular el centroide de un conjunto de puntos respecto a una distancia cualquiera.
-        # En esta función se utiliza la función definida para este problema (delta), pero se espera que funcione para cualquier distancia.
-        # El centroide se calcula como aquel punto p que minimice la suma de distancias \sum delta(p,q_{i}).
-        # Este método es O(n^{2}log n) donde k es tamaño del cluster.
-
-        # La entrada (cluster) se refiere a la agrupación de elementos donde se desea calcular el centroide.
-        # Verificamos que el cluster sea no vacío en caso contrario el centroide se define como el vector (0,0)
-
-        op = ordered_points(points=cluster)
-
-        if cluster:
+    def centroid(S: list) -> list:
         
+        '''
+            Función para calcular el centroide de un conjunto de puntos S respecto a una distancia cualquiera.
+            La implementación se realiza dinámicamente aprovechando delta(p,q)=delta(q,p)
+
+            En esta función se utiliza la función definida para este problema (delta), pero se espera que funcione para cualquier distancia.
+            El centroide se calcula como aquel punto p que minimice la suma de distancias \sum delta(p,q_{i}).
+            Este método es O(k^{3}\log k) donde k es el tamaño de S.
+
+            Verificamos que S sea no vacío en caso contrario el centroide se define como el vector (0,0)
+
+            - Parametros:
+            S: Lista que contiene vectores (representados como listas)
+        
+        '''
+        
+        if S:
+
+            op = ordered_points(points=S) # O(k\log k)
+
             # Inicialización de la distancia mínima 
 
             min_distance = float('inf')
 
             centroid = None
 
-            for p in cluster:
+            # Diccionario para realizar programación dinámica y almacenar distancias ya calculadas.
 
-                sum_distance = sum([delta(p=p, q=q, points=cluster, ordered_points=op) for q in cluster])
+            cache = dict()
+
+            for i in range(len(S)):
+
+                p = S[i]
+
+                sum_distance = 0
+
+                for j in range(len(S)):
+
+                    q = S[j]
+
+                    if (p,q) in cache:
+
+                        distance = cache[(p,q)]
+                    
+                    else:
+
+                        distance = delta(p=p, q=q, ordered_points=op)
+
+                        cache[(p,q)] = distance
+
+                        cache[(q,p)] = cache[(p,q)]
+                
+                    sum_distance += distance
 
                 if sum_distance < min_distance:
                     
@@ -167,7 +218,7 @@ def order_type_clustering(points: list, k: int) -> dict:
 
     # Algoritmo de Lloyd. Devuelve un diccionario para representar los clusters y los centros.
 
-    # op = ordered points (O(n^{2}log n))
+    # op = ordered points
 
     op = ordered_points(points=points)
     
@@ -207,13 +258,13 @@ def order_type_clustering(points: list, k: int) -> dict:
 
         updated_centers = list()
 
-        # Tiempo del siguiente ciclo: O()
+        # Tiempo del siguiente ciclo: O(k^{4}\log k)
 
         for j in range(k):
 
             # Se añade el centroide (respecto a "delta") al nuevo conjunto de centros
 
-            updated_centers.append(centroid(cluster=cluster[j]))
+            updated_centers.append(centroid(cluster[j]))
             
         f += 1
         
@@ -222,7 +273,7 @@ def order_type_clustering(points: list, k: int) -> dict:
         else: centers = updated_centers
 
 
-    print(f'{f} iteraciones.\n')
+    print(f'{f} iteraciones.')
     
     return {'cluster': cluster, 'centers': centers}
 
@@ -294,7 +345,7 @@ def statistics(k: int, num_points: list) -> None:
         plt.legend()
 
         # Agrega una leyenda
-        plt.savefig(r'C:\Users\wamjs\OneDrive\Documentos\Cinvestav\type-order-clustering\figs\\'+str(len(points))+'pts\\'+str(len(points))+'pts_'+str(k)+'clusters.png')
+        plt.savefig(r'C:\Users\wamjs\OneDrive\Documentos\Cinvestav\type-order-clustering\figs\\'+str(len(points))+'pts\\'+str(k)+'clusters\\'+str(len(points))+'pts_'+str(k)+'clusters.png')
         plt.close()
         
         return pts_statistics
@@ -331,79 +382,81 @@ def statistics(k: int, num_points: list) -> None:
 
         stats.to_csv(r'C:\Users\wamjs\OneDrive\Documentos\Cinvestav\type-order-clustering\\'+str(k)+'clusters_statistics.csv')
 
-def blow_up(cluster: list, alpha=1) -> list:
+def cluster_analysis(n: int, k: int, shoulder_arm: int) -> None:
 
-    '''
-        Función para expandir un conjunto de puntos por un parametro alpha
+    def save_expantion(clustering: dict, k: int) -> None:
+
+        '''
+            Función para guardar el estiramiento de cada cluster de un clustering hecho.
+
+            - Parametros:
+            clustering: agrupamiento de un conjunto de puntos. Se recomienda usar order_type_clustering
+            k: número de grupos del cluster.
+        '''
         
-        -Parametros:
-        cluster: conjunto de puntos
-        alpha: factor de estiramiento
-    '''
+        file=open(r'C:\Users\wamjs\OneDrive\Documentos\Python\ruy\rectilinear_crossing_number.pkl','rb')
+        D=pickle.load(file, encoding='latin1')
 
-    sorted_cluster = sorted(cluster, key=lambda vector: vector[0], reverse=True)
-    sup = sorted_cluster[0][0]
-    # Lista para almacenar los vectores q-p
-    pts = list()
-    for p in sorted_cluster:
-        for q in sorted_cluster[sorted_cluster.index(p)+1:]:
-            pts.append([q[0]-p[0], q[1]-p[1]])
-    v = [sum([x[0] for x in pts]) / len(pts), sum([x[1] for x in pts]) / len(pts)]
-    w = [(-1)*v[1], v[0]]
-    
-    # T = transformacion lineal
-    T = [v, [alpha * sup * w[0], alpha * sup * w[1]]]
+        n = 0    
+        for j in clustering['cluster'].keys():
+            n += len(clustering['cluster'][j])
 
-    blow_up = list()
+        # Este programa muestra realiza una transformación lineal de un conjunto de puntos
+        # que infla el conjunto de puntos.
+        for j in clustering['cluster'].keys():
+            cluster = clustering['cluster'][j]
+            if len(cluster) <= 1: continue
+            
+            try: optimal = D[len(cluster)]['pts']
+            except: continue
+            
+            bp = expand(cluster=cluster, alpha=2)
 
-    for p in cluster:
+            # Desempaquetado de las coordenadas en listas separadas
+            xcoordinate, ycoordinate = zip(*bp)
 
-        blow_up.append([(T[0][0]*p[0])+(T[0][1]*p[1]), (T[1][0]*p[0])+(T[1][1]*p[1])])
-    
-    return blow_up
+            # Crea un gráfico de dispersión
+            plt.scatter(xcoordinate, ycoordinate)
 
-def save_blow_up(clustering: dict, k: int) -> None:
+            plt.title(f"Cantidad de Puntos: {len(cluster)}\n"+f"Crossing Number: {PyDCG.crossing.count_crossings_py(cluster)}\n"+f"Optimal Crossing Number: {PyDCG.crossing.count_crossings_py(optimal)}")
 
-    '''
-        Función para guardar el estiramiento de cada cluster de un clustering hecho.
+            plt.savefig(r'C:\Users\wamjs\OneDrive\Documentos\Cinvestav\type-order-clustering\figs\\'+str(n)+'pts\\'+str(k)+'clusters\\'+'cluster_'+str(int(j)+1)+'.png')
 
-        - Parametros:
-        clustering: agrupamiento de un conjunto de puntos. Se recomienda usar order_type_clustering
-        k: número de grupos del cluster.
-    '''
-    
-    file=open(r'C:\Users\wamjs\OneDrive\Documentos\Python\ruy\rectilinear_crossing_number.pkl','rb')
-    D=pickle.load(file, encoding='latin1')
-
-    n = 0    
-    for j in clustering['cluster'].keys():
-        n += len(clustering['cluster'][j])
-
-    # Este programa muestra realiza una transformación lineal de un conjunto de puntos
-    # que infla el conjunto de puntos.
-    for j in clustering['cluster'].keys():
-        cluster = clustering['cluster'][j]
-        if len(cluster) <= 1: continue
+            # Muestra el gráfico
+            #plt.show()
         
-        try: optimal = D[len(cluster)]['pts']
-        except: continue
+        return None
+    
+    def expand(cluster: list, alpha=1) -> list:
+
+        '''
+            Función para expandir un conjunto de puntos por un parametro alpha
+            
+            -Parametros:
+            cluster: conjunto de puntos
+            alpha: factor de estiramiento
+        '''
+
+        sorted_cluster = sorted(cluster, key=lambda vector: vector[0], reverse=True)
+        sup = sorted_cluster[0][0]
+        # Lista para almacenar los vectores q-p
+        pts = list()
+        for p in sorted_cluster:
+            for q in sorted_cluster[sorted_cluster.index(p)+1:]:
+                pts.append([q[0]-p[0], q[1]-p[1]])
+        v = [sum([x[0] for x in pts]) / len(pts), sum([x[1] for x in pts]) / len(pts)]
+        w = [(-1)*v[1], v[0]]
         
-        bp = blow_up(cluster=cluster, alpha=2)
+        # T = transformacion lineal
+        T = [v, [alpha * sup * w[0], alpha * sup * w[1]]]
 
-        # Desempaquetado de las coordenadas en listas separadas
-        xcoordinate, ycoordinate = zip(*bp)
+        blow_up = list()
 
-        # Crea un gráfico de dispersión
-        plt.scatter(xcoordinate, ycoordinate)
+        for p in cluster:
 
-        plt.title(f"Cantidad de Puntos: {len(cluster)}\n"+f"Crossing Number: {PyDCG.crossing.count_crossings_py(cluster)}\n"+f"Optimal Crossing Number: {PyDCG.crossing.count_crossings_py(optimal)}")
-
-        plt.savefig(r'C:\Users\wamjs\OneDrive\Documentos\Cinvestav\type-order-clustering\figs\\'+str(n)+'pts\\'+str(k)+'clusters\\'+'cluster_'+str(int(j)+1)+'.png')
-
-        # Muestra el gráfico
-        plt.show()
-
-def sholuders_arms(n: int, k: int, shoulder_arm: int) -> None:
+            blow_up.append([(T[0][0]*p[0])+(T[0][1]*p[1]), (T[1][0]*p[0])+(T[1][1]*p[1])])
+        
+        return blow_up
 
     '''
         Función para realizar un clustering en tipo de orden a un cluster.
@@ -420,15 +473,34 @@ def sholuders_arms(n: int, k: int, shoulder_arm: int) -> None:
         f.close()
     except: raise('No existe archivo de clustering.')
 
+    save_expantion(clustering=clustering, k=k)
     # Brazo a analizar del clustering hecho para el conjunto de n puntos en k clusters.
     arm = clustering['cluster'][str(shoulder_arm-1)]
 
     # otc = order_type_clustering
-    otc = order_type_clustering(blow_up(arm), k)
+    otc = order_type_clustering(expand(arm), k)
+    # Lectura de puntos
+
+    file=open(r'C:\Users\wamjs\OneDrive\Documentos\Python\ruy\rectilinear_crossing_number.pkl','rb')
+
+    D=pickle.load(file, encoding='latin1')
 
     for i in range(k):
 
         cluster = otc['cluster'][i]
+
+        try: pts = D[len(cluster)]['pts']
+        except: continue
+
+        optimal = PyDCG.crossing.count_crossings_py(pts)
+        crossing = PyDCG.crossing.count_crossings_py(cluster)
+
+        print(f'\nNum. Puntos Cluster {i+1}: {len(cluster)}')
+        print(f'\nCrossing Number Cluster {i+1}: {crossing}')
+        print(f'\nOptimal Crossing Number {i+1}: {optimal}')
+        print(f'\nRazón Número Puntos y Cluster {i+1}: {round(len(cluster) / len(pts),3)}')
+        if crossing == 0: print(f'\nRazón Óptimo y Número Cruce {i+1}: 1\n')
+        else: print(f'\nRazón Óptimo y Número Cruce {i+1}: {round(optimal / crossing,3)}\n')
 
         # Desempaquetado de las coordenadas en listas separadas
         xcoordinate, ycoordinate = zip(*cluster)
